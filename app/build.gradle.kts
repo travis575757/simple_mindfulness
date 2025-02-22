21b9796185ef03e2080fa4c952da60c7a1c58c36
travis575757 <tfrink238@gmail.com>
2025-02-22 10:43:49 -0500

    Version 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.vtrifidgames.simplemindfulnesstimer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
