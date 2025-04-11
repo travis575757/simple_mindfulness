df0df16389abfa0518a39d7ef3a312bd44002d44
travis575757 <tfrink238@gmail.com>
2025-04-11 19:50:28 -0400

    Adding ability to cancel a session

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.vtrifidgames.simplemindfulnesstimer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
